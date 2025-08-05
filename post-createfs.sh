bf444bfd555bae5f75402c5d4bb0d3d3fdddfb12
Fernando Mumbach <fermuch@ayvu.net>
2025-08-05 14:43:54 -0300

    minimal config for fwup

--- post-createfs.sh
+++ post-createfs.sh
@@ -9,6 +9,28 @@ if [ -f "$HOST_DIR/bin/mkenvimage" ] && [ -f "$NERVES_DEFCONFIG_DIR/uboot/uboot.
     echo "Generated uboot-env.bin in images directory"
 fi
 
+# Create boot.vfat
+# if [ -f "$BINARIES_DIR/fip.bin" ] && [ -f "$BINARIES_DIR/boot.sd" ] && [ -f "$BINARIES_DIR/uboot-env.bin" ]; then
+#     echo "Creating boot.vfat image..."
+    
+#     # Create temporary directory for boot files
+#     BOOT_DIR=$(mktemp -d)
+#     cp $BINARIES_DIR/fip.bin $BOOT_DIR/
+#     cp $BINARIES_DIR/boot.sd $BOOT_DIR/
+#     cp $BINARIES_DIR/uboot-env.bin $BOOT_DIR/
+    
+#     # Create 16MB FAT32 image
+#     $HOST_DIR/sbin/mkfs.fat -C -F 32 -n "boot" $BINARIES_DIR/boot.vfat 16384
+    
+#     # Copy files into the FAT image
+#     $HOST_DIR/bin/mcopy -i $BINARIES_DIR/boot.vfat $BOOT_DIR/* ::/
+    
+#     # Cleanup
+#     rm -rf $BOOT_DIR
+    
+#     echo "Generated boot.vfat image"
+# fi
+
 # Run the common post-image processing for nerves
 FWUP_CONFIG=$NERVES_DEFCONFIG_DIR/fwup.conf
 $BR2_EXTERNAL_NERVES_PATH/board/nerves-common/post-createfs.sh $TARGET_DIR $FWUP_CONFIG
