f3d883f1881e8763fd91a0a4f564053947981fe6
Fernando Mumbach <fermuch@ayvu.net>
2025-08-07 11:45:21 -0300

    setting ID on startup

--- rootfs-overlay/usr/share/on-start.sh
+++ rootfs-overlay/usr/share/on-start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-# TODO: set up data partition on first boot
+# Check if nerves_serial_number exists, if not create it
+if ! fw_printenv nerves_serial_number > /dev/null 2>&1; then
+  ID=$(uuidgen -r | tr -d '-')
+  fw_setenv nerves_serial_number "$ID"
+  echo "Setting nerves_serial_number to: $ID"
+fi
