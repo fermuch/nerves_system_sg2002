b421a069a5ad4dce27351c38052b12786bd2cd14
Fernando Mumbach <fermuch@ayvu.net>
2025-08-07 15:23:35 -0300

    adding several utils

--- rootfs-overlay/usr/share/on-start.sh
+++ rootfs-overlay/usr/share/on-start.sh
@@ -2,7 +2,13 @@
 
 # Check if nerves_serial_number exists, if not create it
 if ! fw_printenv nerves_serial_number > /dev/null 2>&1; then
-  ID=$(uuidgen -r | tr -d '-')
+  UUID=$(uuidgen -r)
+  ID=""
+  for char in $(echo "$UUID" | fold -w1); do
+    if [ "$char" != "-" ]; then
+      ID="$ID$char"
+    fi
+  done
   fw_setenv nerves_serial_number "$ID"
   echo "Setting nerves_serial_number to: $ID"
 fi
