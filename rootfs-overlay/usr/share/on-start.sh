b1675d4fc8d01081ad73d931b662bb8681c58adc
Fernando Mumbach <fermuch@ayvu.net>
2025-08-12 09:34:21 -0300

    starting sensor and loading modules

--- rootfs-overlay/usr/share/on-start.sh
+++ rootfs-overlay/usr/share/on-start.sh
@@ -1,23 +1,41 @@
 #!/bin/sh
 
-# Initialize mdev for hotplug/coldplug and load core Wi-Fi modules
-echo /sbin/mdev > /proc/sys/kernel/hotplug
-/sbin/mdev -s
+# # Initialize mdev for hotplug/coldplug and load core Wi-Fi modules
+# echo /sbin/mdev > /proc/sys/kernel/hotplug
+# /sbin/mdev -s
 
-# Try to load Wi-Fi stack (ignore errors if already built-in)
-/sbin/modprobe cfg80211 2>/dev/null || true
-/sbin/modprobe mac80211 2>/dev/null || true
-/sbin/modprobe brcmfmac 2>/dev/null || true
+# Set up Sensor (camera)
+GPIO_SENSOR_PWR=358
+if [ ! -d /sys/class/gpio/gpio$GPIO_SENSOR_PWR ]; then
+  echo $GPIO_SENSOR_PWR > /sys/class/gpio/export
+  echo "out" > /sys/class/gpio/gpio$GPIO_SENSOR_PWR/direction
+fi
+if [ -f /sys/class/gpio/gpio$GPIO_SENSOR_PWR/value ]; then
+  echo 1 > /sys/class/gpio/gpio$GPIO_SENSOR_PWR/value
+fi
 
-# Load CVI camera/video kernel modules
-/sbin/modprobe cv181x_sys 2>/dev/null || true
-/sbin/modprobe cv181x_base 2>/dev/null || true
-/sbin/modprobe snsr_i2c 2>/dev/null || true
-/sbin/modprobe cvi_mipi_rx 2>/dev/null || true
-/sbin/modprobe cv181x_vi 2>/dev/null || true
-/sbin/modprobe cv181x_vpss 2>/dev/null || true
-/sbin/modprobe cv181x_cif 2>/dev/null || true
-/sbin/modprobe cvi_vc_driver 2>/dev/null || true
+# Install base kernel modules
+insmod /usr/share/modules/cv181x_sys.ko
+insmod /usr/share/modules/cv181x_base.ko
+insmod /usr/share/modules/cv181x_rtos_cmdqu.ko
+insmod /usr/share/modules/cv181x_fast_image.ko
+insmod /usr/share/modules/cvi_mipi_rx.ko
+insmod /usr/share/modules/snsr_i2c.ko
+insmod /usr/share/modules/cv181x_vi.ko vi_log_lv=1
+insmod /usr/share/modules/cv181x_vpss.ko vpss_log_lv=1
+insmod /usr/share/modules/cv181x_dwa.ko
+insmod /usr/share/modules/cv181x_vo.ko vo_log_lv=1
+insmod /usr/share/modules/cv181x_mipi_tx.ko
+insmod /usr/share/modules/cv181x_rgn.ko
+insmod /usr/share/modules/cv181x_clock_cooling.ko
+insmod /usr/share/modules/cv181x_tpu.ko
+insmod /usr/share/modules/cv181x_vcodec.ko
+insmod /usr/share/modules/cv181x_jpeg.ko
+insmod /usr/share/modules/cvi_vc_driver.ko MaxVencChnNum=9 MaxVdecChnNum=9
+insmod /usr/share/modules/cv181x_ive.ko
+insmod /usr/share/modules/cfg80211.ko
+insmod /usr/share/modules/brcmutil.ko
+insmod /usr/share/modules/brcmfmac.ko
 
 # Check if nerves_serial_number exists, if not create it
 if ! fw_printenv nerves_serial_number > /dev/null 2>&1; then
