ffc2464978228d462207bb0c035884fc8fa9d204
Fernando Mumbach <fermuch@ayvu.net>
2025-08-18 09:17:19 -0300

    minor fix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,10 @@
 with import <nixpkgs> {}; # This should probably be pinned to something. For me it points to 24.05 channel
 let
+  otp = beam28Packages;
   basePackages = [
-    beam28Packages.elixir_1_18
-    beam28Packages.erlang
-    beam28Packages.elixir-ls
+    otp.elixir_1_18
+    otp.erlang
+    otp.elixir-ls
   ];
   PROJECT_ROOT = builtins.toString ./.;
 
@@ -12,10 +13,13 @@ let
     mkdir -p .nix-hex
     export MIX_HOME=${PROJECT_ROOT}/.nix-mix
     export HEX_HOME=${PROJECT_ROOT}/.nix-hex
-    export PATH=$MIX_HOME/bin:$PATH
+    export MIX_PATH="${otp.hex}/lib/erlang/lib/hex/ebin"
+    export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
     export PATH=$HEX_HOME/bin:$PATH
     export LANG=en_NZ.UTF-8
     export ERL_AFLAGS="-kernel shell_history enabled"
+
+    mix local.hex --force
     '';
 
   in mkShell {
